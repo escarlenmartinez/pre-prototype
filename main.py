bdbb56d2c7dc33d86a1d8574d1f702ee939970c4
escarlenmartinez <72760150+escarlenmartinez@users.noreply.github.com>
2020-11-28 13:33:33 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
     print("Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + "°F" + " - " + input.temperature(TemperatureUnit.CELSIUS) + "°C")
-    if input.temperature(TemperatureUnit.FAHRENHEIT) >= 100:
-        light.set_all(light.rgb(200,135,227))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 100:
-        light.set_all(light.rgb(260,0,15))
+    if input.temperature(TemperatureUnit.FAHRENHEIT) >= 65:
+        light.set_all(light.rgb(122,73,196))
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 60:
+        light.set_all(light.rgb(255,0,0))
     else:
-        light.set_all(light.rgb(209,201,201))
+        light.set_all(light.rgb(76,182,185))
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 while (true) {
     console.log("Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + "°F" + " - " + input.temperature(TemperatureUnit.Celsius) + "°C")
-    if (input.temperature(TemperatureUnit.Fahrenheit) >= 100) {
-        light.setAll(light.rgb(200, 135, 227))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 100) {
-        light.setAll(light.rgb(260, 0, 15))
+    if (input.temperature(TemperatureUnit.Fahrenheit) >= 65) {
+        light.setAll(light.rgb(122, 73, 196))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 60) {
+        light.setAll(light.rgb(255, 0, 0))
     } else {
-        light.setAll(light.rgb(209, 201, 201))
+        light.setAll(light.rgb(76, 182, 185))
     }
     
 }
